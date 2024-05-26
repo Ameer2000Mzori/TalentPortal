dd8944430a4b38dfb896cd1e2a274377dbe3dbf4
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-05-26 21:06:47 +0200

    added some text and coloring for the navbar

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+
 export default function Header() {
-  return <div>header</div>
+  return (
+    <div>
+      <div className="text-[#000000] h-[73px] ">
+        <h1 className="text-[#013a9bff] ">
+          TALENT <i>PORTAL</i>
+        </h1>
+
+        <Link href={'/'}>
+          <h1>Home</h1>
+        </Link>
+      </div>
+      <div></div>
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Header from './header/page'
+
 export default function Home() {
   return (
     <div>
+      <Header />
       <div>home page</div>
     </div>
   )
