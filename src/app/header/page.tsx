74296388da724a27c7b0eca13e998c23c029ddf1
Ameer Ameen <ameerapex496@gmail.com>
2024-05-26 22:30:09 +0200

    added more links to the navbar

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <div className="pl-[24px]  pr-[24px] text-[#000000] h-[73px] bg-white flex flex-row text-center items-center justify-center">
-      <div className=" h-[100%] w-[75%] flex flex-row text-center items-center justify-between">
+      <div className=" h-[100%] w-[100%] xl:w-[70%] flex flex-row text-center items-center justify-between">
         <div className="flex flex-row text-center items-center justify-between gap-[22px] font leading-[24px] text-[14px] h-[100%]">
           <Link href={'/'}>
             <h1 className=" text-[24px] flex flex-col justify-center text-start  text-[#013a9bff] font-bold">
@@ -15,7 +15,13 @@ export default function Header() {
             <h1>Home</h1>
           </Link>
         </div>
-        <div>
+        <div className="flex flex-row text-center items-center justify-between gap-[22px] h-[100%] font leading-[24px] text-[14px] ">
+          <Link href="/">
+            <h1 className="text-[14px] text-[#2d2d2d] leading-[21px] underline">
+              create account
+            </h1>
+          </Link>
+          <div className="h-[60%] w-[1px] bg-slate-300"></div>
           <Link href="/">
             <h1 className="text-[14px] text-[#2d2d2d] leading-[21px] underline">
               post job!
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      screens: {
+        xs: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '1xl': '1350px',
+        '2xl': '1536px',
+        '3xl': '2048px',
+        '4xl': '2560px',
+        '5xl': '3072px',
+        '6xl': '3584px',
+        '7xl': '4096px',
+        '8xl': '4608px',
+      },
+
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
