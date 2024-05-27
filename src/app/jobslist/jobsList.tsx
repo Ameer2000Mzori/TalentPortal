5bc0d98dbaf9d396b335df83399cef920ab722a0
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-05-27 15:38:23 +0200

    added more logic to list the jobs list data

--- src/app/jobslist/jobsList.tsx
+++ src/app/jobslist/jobsList.tsx
@@ -8,7 +8,10 @@ export default function JobsList() {
           className="h-[100px] w-[100%] border border-[#cacaca] mt-[12px] rounded-[7px]"
         >
           <h1>{job.title}</h1>
+          <p>{job.company}</p>
+          <p>{job.location}</p>
           <p>{job.description}</p>
+          <p>{job.date}</p>
         </div>
       ))}
     </div>
