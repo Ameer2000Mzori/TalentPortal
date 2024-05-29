e775fbcd3392b603b8ad88036605ebfedde38918
Ameer Ameen <ameerapex496@gmail.com>
2024-05-29 17:47:06 +0200

    added badge, for each job that is listed

--- src/app/jobs/jobs.tsx
+++ src/app/jobs/jobs.tsx
@@ -5,7 +5,7 @@ export default function JobsPage() {
   return (
     <div className="h-[100vh] bg-white">
       <div className="h-[95%] flex flex-row text-center items-center justify-center gap-[25px]">
-        <div className="w-[20%] h-[92%] overflow-hidden">
+        <div className="w-[25%] h-[92%] overflow-hidden">
           <JobsList />
         </div>
         <div className="w-[30%] h-[90%] border border-[#949494] ">
--- src/app/jobslist/jobsList.tsx
+++ src/app/jobslist/jobsList.tsx
@@ -1,3 +1,4 @@
+import Badge from '../shared/badge'
 import { jobsData } from './jobsData'
 export default function JobsList() {
   return (
@@ -5,9 +6,10 @@ export default function JobsList() {
       {jobsData.map((job, index) => (
         <div
           key={index}
-          className=" hover:underline cursor-pointer relative pt-[15px] h-[220px] w-[100%] border border-[#cacaca] mt-[12px] rounded-[7px]"
+          className=" hover:shadow-[5px_-5px_5px_5px_rgba(45,78,255,0.15)] transition-all duration-300 ease-in-out cursor-pointer relative pt-[15px] h-[300px] w-[100%] border border-[#cacaca] mt-[12px] rounded-[7px]"
         >
-          <div className="flex flex-col w-[100%] h-[100%]  justify-start text-start items-start gap-[5px] pr-[6px] pl-[6px] pt-[5px] pb-[5px] overflow-hidden">
+          <div className="flex flex-col w-[100%] h-[100%]  justify-start text-start items-start gap-[5px] pr-[6px] pl-[16px] pt-[5px] pb-[5px] overflow-hidden">
+            <Badge data="new" />
             <p className="font-bold text-[19px]">
               {job.company}, {job.title}
             </p>
@@ -15,7 +17,7 @@ export default function JobsList() {
           </div>
           <div className=" absolute flex flex-row left-[5px] pr-[5px] pl-[5px] bottom-[5px] text-start items-center justify-center gap-[5px] text-[11px] text-slate-300">
             <p>{job.location}</p>{' '}
-            <p className="text-[9px] p-0 m-0">{job.date}</p>
+            <p className="text-[15px] p-0 m-0">{job.date}</p>
           </div>
         </div>
       ))}
